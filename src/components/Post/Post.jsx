81161abeef03f092e33b854eae4eb285af116427
Aminul667 <rahathssn@gmail.com>
2023-04-04 21:28:40 +0600

    54-6 (Recap) Dynamic Data loader and navigate on button click

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Post.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
     const {id, title, body} = post;
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate(`/post/${id}`);
+    }
+    
     return (
         <div className='post'>
             <h5>ID: {id}</h5>
             <h4>Title: {title}</h4>
             <Link to={`/post/${id}`}>Show Details</Link>
-            <button>Show post details</button>
+            <Link to={`/post/${id}`}><button>Show post details</button></Link>
+            <button onClick={handleNavigation}>With Button Handler</button>
         </div>
     );
 };
