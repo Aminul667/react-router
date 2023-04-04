ac5e5632b5195ecef7568375bd543a8beae4c049
Aminul667 <rahathssn@gmail.com>
2023-04-04 16:05:48 +0600

    54-4 Display users, create dynamic link with route parameter

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
-    const data = useLoaderData();
-    console.log(data);
-    
+    const friends = useLoaderData();
+
     return (
         <div>
-            <h2>This is a friends component</h2>
+            <h2>These are my friends: {friends.length}</h2>
+            {
+                friends.map(friend => <Friend
+                    key={friend.id}
+                    friend={friend}
+                ></Friend>)
+            }
         </div>
     );
 };
